915554e8fbf13260e0548102119f429744d5db76
Smitha <smitha.nagaraj@gmail.com>
2026-06-30 20:15:38 +0530

    Final clean terraform eks setup for jenkins

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,7 @@
 output "cluster_name" {
-  value = module.eks_al2023.cluster_name
+  value = module.eks.cluster_name
 }
 
 output "cluster_endpoint" {
-  value = module.eks_al2023.cluster_endpoint
-}
-
-output "cluster_version" {
-  value = module.eks_al2023.cluster_version
-}
-
-output "vpc_id" {
-  value = module.vpc.vpc_id
+  value = module.eks.cluster_endpoint
 }
